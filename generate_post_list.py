8b3ef939d7fcef480c49cc81a70feb28aa7f6cbe
Dotmonger <dotmong3r@gmail.com>
2016-12-28 20:56:14 +0700

    reversepost list generator

--- generate_post_list.py
+++ generate_post_list.py
@@ -8,7 +8,7 @@ target_file = 'posts.json'
 def getfiles(dirpath):
     a = [s for s in os.listdir(dirpath)
          if os.path.isfile(os.path.join(dirpath, s))]
-    a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)))
+    a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)), reverse=True)
     return a
 
 onlyfiles = getfiles(mypath)
